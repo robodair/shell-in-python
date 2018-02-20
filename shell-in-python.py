308d36525738c67d612619853a502db4ff5e3a86
Alisdair <alisdair.w.robertson@gmail.com>
2018-02-20 22:51:24 +1100

    Refactor to OO

--- shell-in-python.py
+++ shell-in-python.py
@@ -3,14 +3,37 @@
 import os
 import sys
 
-def main():
-    while True:
-        prompt = os.getcwd() + " > "
-        command = input(prompt)
-        print(command)
+class BuiltinExit(object):
 
-        if command == "exit":
-            sys.exit()
+    this_cmd = "exit"
+
+    @classmethod
+    def is_command(cls, cmd):
+        if cmd == cls.this_cmd:
+            return True
+        return False
+
+    def execute(self, cmd):
+        # TODO: Check if there's the right number of args or not
+        sys.exit(0)
+
+
+class InteractiveREPL(object):
+
+    builtin_commands = [BuiltinExit]
+
+    def run(self):
+        while True:
+            prompt = os.getcwd() + " > "
+            command = input(prompt)
+
+            for builtin in self.builtin_commands:
+                if builtin.is_command(command):
+                    builtin().execute(command)
+
+            print(command)
+
 
 if __name__ == "__main__":
-    main()
+    repl = InteractiveREPL()
+    repl.run()
