8d398068badc435f82053d8e3102656855a04b45
Alisdair <alisdair.w.robertson@gmail.com>
2018-02-20 22:42:23 +1100

    Add builtin exit command

--- shell-in-python.py
+++ shell-in-python.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+
 def main():
     while True:
         prompt = os.getcwd() + " > "
         command = input(prompt)
         print(command)
+
+        if command == "exit":
+            sys.exit()
 
 if __name__ == "__main__":
     main()
